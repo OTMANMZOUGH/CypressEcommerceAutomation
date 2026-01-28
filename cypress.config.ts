ba80941e22b576e9ec6dcbba4fe2980a09346c45
otman <mzoughotman12@gmail.com>
2026-01-28 23:13:34 +0100

    Add base URL configuration to Cypress environment settings

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  env: {
+    url: "https://rahulshettyacademy.com"
+  },
   e2e: {
     // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
