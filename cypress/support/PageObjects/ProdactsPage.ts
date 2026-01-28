4308290a584df5ce755223288a411d51b7af7647
otman <mzoughotman12@gmail.com>
2026-01-28 23:12:38 +0100

    Add ProdactsPage Page Object with methods for product validation and interactions

---
+++ cypress/support/PageObjects/ProdactsPage.ts
@@ -0,0 +1,28 @@
+import CartPage from "./CartPage";
+
+class ProdactsPage {
+
+    pageValidation(){
+        cy.contains('Shop Name').should('be.visible')
+    }
+    verifyCardLimite(){
+        cy.get('app-card').should('have.length', 4)
+    }
+    goToCar(){
+        cy.contains('a','Checkout').click()
+        return new CartPage()
+    }
+    addProdact(prodactName){
+
+
+        //cy.get('app-card').filter(`:contains("${prodactName}")`)
+        cy.get('app-card').filter(':contains("' + prodactName + '")')
+            .then(($filteredCard) => {
+                //cy.wrap($filteredCard).should('have,length', 1)
+                cy.wrap($filteredCard).contains('button','Add').click()
+                //cy.wrap($filteredCard).find('button').contains('Add').click()
+            })
+        cy.get('app-card').eq(0).contains('button','Add').click()
+    }
+}
+export default ProdactsPage;
