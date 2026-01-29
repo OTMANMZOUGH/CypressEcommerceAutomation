51536a549d0ed98517ef6c822e02634a1d0d532e
otman <mzoughotman12@gmail.com>
2026-01-30 00:22:29 +0100

    Add mochawesome reporter and project ID to Cypress configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   env: {
     url: "https://rahulshettyacademy.com"
   },
+ // retries:{
+    // runMode: 1, //1 test failed tests
+  // },
+  projectId: "z89ekw",
   e2e: {
-    // Configure your E2E tests here
+    setupNodeEvents(on, config) {
+      // Intégration du plugin pour générer le rapport après les tests
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
   },
-})
+});
